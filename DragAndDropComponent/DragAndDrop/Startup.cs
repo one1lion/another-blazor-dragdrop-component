61f3a90b3cf3a6e3cad00fc4851a4b794ac3dfc3
one1lion <i_am_listening@Hotmail.com>
2019-12-27 11:29:00 -0700

    Setup basic html structure for handling draggable items.  Needed to implement a js hack since Firefox (and possibly other browsers) require a call to dataTransfer.setData(...)

--- DragAndDropComponent/DragAndDrop/Startup.cs
+++ DragAndDropComponent/DragAndDrop/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using DragAndDrop.Data;
 
 namespace DragAndDrop {
   public class Startup {
@@ -24,7 +23,6 @@ namespace DragAndDrop {
     public void ConfigureServices(IServiceCollection services) {
       services.AddRazorPages();
       services.AddServerSideBlazor();
-      services.AddSingleton<WeatherForecastService>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ DragAndDropComponent/DragAndDrop/wwwroot/lib/draganddrop-compat.js
@@ -0,0 +1,35 @@
+﻿/* 
+ * This uses MutationObserver to detect DOM changes (since page navigation in Razor 
+ * does not fire load/unload events) to add the ondragstart event. Sample adapted from
+ * https://stackoverflow.com/questions/3219758/detect-changes-in-the-dom
+ */
+var observeDOM = (function () {
+  var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+  return function (obj, callback) {
+    if (!obj || !obj.nodeType === 1) return;
+
+    if (MutationObserver) {
+      var obs = new MutationObserver(function (mutations, observer) {
+        callback(mutations);
+      });
+      obs.observe(obj, { childList: true, subtree: true });
+    } else if (window.addEventListener) {
+      obj.addEventListener('DOMNodeInserted', callback, false);
+      obj.addEventListener('DOMNodeRemoved', callback, false);
+    }
+  };
+})();
+
+observeDOM(document, function (m) {
+  var dnds = document.getElementsByClassName("draganddrop-item");
+  for (var i = 0; i < dnds.length; i++) {
+    dnds[i].ondragstart = function (e) {
+      e.dataTransfer.setData("text/plain", "Dragging");
+    };
+    dnds[i].ondrop = function (e) {
+      e.preventDefault();
+      return false;
+    };
+  }
+});
