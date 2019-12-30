b7d13f7dfff2362245d4691663cf3d299e250a9a
one1lion <i_am_listening@Hotmail.com>
2019-12-30 10:05:53 -0700

    Split the sample into two types - one with div, one with ul.  Finished the logic and structure for re-ordering while dragging (live-reorder)

--- DragAndDropComponent/DragAndDrop/wwwroot/lib/draganddrop-compat.js
+++ DragAndDropComponent/DragAndDrop/wwwroot/lib/draganddrop-compat.js
@@ -3,10 +3,12 @@
  * does not fire load/unload events) to add the ondragstart event. Sample adapted from
  * https://stackoverflow.com/questions/3219758/detect-changes-in-the-dom
  */
+var doSet = true;
 var observeDOM = (function () {
   var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
   return function (obj, callback) {
+    if (!doSet) { return; }
     if (!obj || !obj.nodeType === 1) return;
 
     if (MutationObserver) {
@@ -22,10 +24,16 @@ var observeDOM = (function () {
 })();
 
 observeDOM(document, function (m) {
+  if (!doSet) { return; }
   var dnds = document.querySelectorAll("*[draggable=true]");
   for (var i = 0; i < dnds.length; i++) {
     dnds[i].ondragstart = function (e) {
-      e.dataTransfer.setData("text/plain", "Dragging");
+      try {
+        e.dataTransfer.setData("text/plain", "Dragging");
+      } catch (e) {
+        doSet = false;
+        return;
+      }
     };
     dnds[i].ondrop = function (e) {
       e.preventDefault();
