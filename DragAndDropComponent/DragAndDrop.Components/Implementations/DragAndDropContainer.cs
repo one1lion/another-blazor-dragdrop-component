886f0f0bb4eddc7109168e889df635c09adbc00d
one1lion <i_am_listening@Hotmail.com>
2020-01-03 09:52:59 -0700

    Removed generic types on interfaces and refined implementations; updated example on Index

--- DragAndDropComponent/DragAndDrop.Components/Implementations/DragAndDropContainer.cs
+++ DragAndDropComponent/DragAndDrop.Components/Implementations/DragAndDropContainer.cs
@@ -1,11 +1,25 @@
 ﻿using DragAndDrop.Components.Interfaces;
+using System;
 using System.Collections.Generic;
 
 namespace DragAndDrop.Components {
-  public class DragAndDropContainer<T> : IDragAndDropContainer<T> {
+  /// <summary>
+  /// An implementation of <see cref="DragAndDrop.Components.Interfaces.IDragAndDropContainer"/>
+  /// that allows <see cref="DragAndDrop.Components.Interfaces.IDragAndDropElement"/>s to be
+  /// added as child elements
+  /// </summary>
+  public class DragAndDropContainer : IDragAndDropContainer {
+    public DragAndDropContainer() {
+      Id = Guid.NewGuid().ToString();
+    }
+
+    /// <inheritdoc cref="DragAndDrop.Components.Interfaces.IDragAndDropElement.Id" />
+    public string Id { get; }
+    /// <inheritdoc cref="DragAndDrop.Components.Interfaces.IDragAndDropElement.Name" />
     public string Name { get; set; }
-    public int Order { get; set; }
-    public IDragAndDropContainer<T> Parent { get; set; }
-    public IList<IDragAndDropElement<T>> Children { get; set; }
+    /// <inheritdoc cref="DragAndDrop.Components.Interfaces.IDragAndDropElement.Parent" />
+    public IDragAndDropContainer Parent { get; set; }
+    /// <inheritdoc cref="DragAndDrop.Components.Interfaces.IDragAndDropContainer.Children" />
+    public IList<IDragAndDropElement> Children { get; set; }
   }
 }
--- DragAndDropComponent/DragAndDrop.Components/Interfaces/IDraggableElement.cs
+++ DragAndDropComponent/DragAndDrop.Components/Interfaces/IDraggableElement.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 
 namespace DragAndDrop.Components.Interfaces {
   /// <summary>
-  /// An <see cref="DragAndDrop.Components.Interfaces.IDragAndDropElement{T}"/> that is
+  /// An <see cref="DragAndDrop.Components.Interfaces.IDragAndDropElement"/> that is
   /// specified to be draggable
   /// </summary>
-  /// <typeparam name="T">The type of data contained within the element</typeparam>
-  public interface IDraggableElement<T> : IDragAndDropElement<T> {
+  /// <remarks>
+  /// An element that only implements this interface cannot contain children by default.
+  /// To allow children, also implement the 
+  /// <see cref="DragAndDrop.Components.Interfaces.IDragAndDropContainer"/> interface.
+  /// </remarks>
+  public interface IDraggableElement : IDragAndDropElement {
     /// <summary>
     /// A flag indicating whether or not this element should 
     /// be allowed to be dragged currently
@@ -16,7 +18,7 @@ namespace DragAndDrop.Components.Interfaces {
     bool DragEnabled { get; set; }
     /// <summary>
     /// A list of target 
-    /// <see cref="DragAndDrop.Components.Interfaces.IDragAndDropElement{T}.Name" /> 
+    /// <see cref="DragAndDrop.Components.Interfaces.IDragAndDropElement.Name" />s
     /// the item can be dropped into/onto
     /// </summary>
     List<string> AllowedTargetNames { get; set; }
@@ -25,18 +27,18 @@ namespace DragAndDrop.Components.Interfaces {
     /// Indicates whether the current element can be dropped into or onto a specified target name
     /// </summary>
     /// <param name="container"> 
-    /// The <see cref="DragAndDrop.Components.Interfaces.IDragAndDropContainer{T}.Name" /> to test
+    /// The <see cref="DragAndDrop.Components.Interfaces.IDragAndDropContainer.Name" /> to test
     /// whether this element can be dropped into/onto
     /// </param>
     /// <returns>Whether or not this element can be dropped into/onto the specified target name</returns>
-    public bool CanDrop(IDragAndDropContainer<T> container) => container is { } && AllowedTargetNames is { } && AllowedTargetNames.Contains(container.Name);
+    public bool CanDrop(IDragAndDropContainer container) => container is { } && AllowedTargetNames is { } && AllowedTargetNames.Contains(container.Name);
 
     // TODO: Remove this when ready to implement
-    //void GroupWith(IDragAndDropElement<T> element);
+    //void GroupWith(IDragAndDropElement element);
 
 
 
-    //public bool MoveInto(IDragAndDropContainer<T> container, int? order = default) {
+    //public bool MoveInto(IDragAndDropContainer container, int? order = default) {
     //  if (container is null || !CanDrop(container)) { return false; }
 
     //  if(container.Id == Parent?.Id) {
@@ -46,7 +48,7 @@ namespace DragAndDrop.Components.Interfaces {
     //  }
     //  Parent = container;
     //  // Get all draggable
-    //  var elemChildren = ((List<IDraggableElement<T>>)container.Children.Where(x => x.GetType().GetTypeInfo().IsAssignableFrom(typeof(IDraggableElement<T>).GetTypeInfo())).Select(x => (IDraggableElement<T>)x).OrderBy(x => x.Order));
+    //  var elemChildren = ((List<IDraggableElement>)container.Children.Where(x => x.GetType().GetTypeInfo().IsAssignableFrom(typeof(IDraggableElement).GetTypeInfo())).Select(x => (IDraggableElement)x).OrderBy(x => x.Order));
     //  for(var i = 0; i < elemChildren.Count(); i++) {
     //    elemChildren[i].Order = i;
     //  }
