9cf06df9f43d0f3c7860e361e4daffe36cddd423
one1lion <i_am_listening@Hotmail.com>
2020-01-14 08:32:36 -0700

    Made a non-generic version of DragAndDropItem/DraggableItem that uses a dynamic variable for the Item; replaced the ViewModel for the two corresponding components

--- DragAndDropComponent/DragAndDrop.Components/Implementations/DraggableItemViewModel.cs
+++ DragAndDropComponent/DragAndDrop.Components/Implementations/DraggableItemViewModel.cs
@@ -7,6 +7,27 @@ namespace DragAndDrop.Components {
   /// An implementation of <see cref="DragAndDrop.Components.Interfaces.IDraggableElement"/>
   /// that can be dragged onto/into <see cref="DragAndDrop.Components.Interfaces.IDragAndDropContainer" />s
   /// </summary>
+  public class DraggableItemViewModel : DragAndDropItemViewModel, IDraggableElement {
+    /// <summary>The default constructor</summary>
+    public DraggableItemViewModel() : base() { }
+
+    /// <inheritdoc cref="DragAndDrop.Components.Interfaces.IDraggableElement.AllowedTargetNames" />
+    public List<string> AllowedTargetNames { get; set; } = new List<string>();
+
+    /// <inheritdoc cref="DragAndDrop.Components.Interfaces.IDraggableElement.DragEnabled" />
+    public bool DragEnabled { get; set; } = true;
+
+    /// <inheritdoc cref="DragAndDrop.Components.Interfaces.IDragAndDropElement.Clone"/>
+    public new DraggableItemViewModel Clone() {
+      return ((IDragAndDropElement)this).Clone<DraggableItemViewModel>();
+    }
+
+  }
+
+/// <summary>
+  /// An implementation of <see cref="DragAndDrop.Components.Interfaces.IDraggableElement"/>
+  /// that can be dragged onto/into <see cref="DragAndDrop.Components.Interfaces.IDragAndDropContainer" />s
+  /// </summary>
   /// <typeparam name="T">The type of data contained within the element</typeparam>
   public class DraggableItemViewModel<T> : DragAndDropItemViewModel<T>, IDraggableElement {
     /// <summary>The default constructor</summary>
