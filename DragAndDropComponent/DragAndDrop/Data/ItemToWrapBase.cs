75de044f1b46a2480fa799f5c66165272bd785f7
Humberto Cruz-Oyola Jr <i_am_listening@Hotmail.com>
2020-02-04 14:27:39 -0700

    Added DragonDrop project pieces from work project

---
+++ DragAndDropComponent/DragAndDrop/Data/ItemToWrapBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DragAndDrop.Data {
+  public class ItemToWrapBase {
+    public ItemToWrapBase() {
+      Id = Guid.NewGuid().ToString();
+    }
+    public string Id { get; set; }
+    public ItemToWrapGroup Parent { get; set; }
+
+  }
+}
---
+++ DragAndDropComponent/DragonDrop/Lib/Utils.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace DragonDrop.Lib {
+  public class Utils {
+    public static T SimpleClone<T>(T obj) {
+      // If T is one of the known primitive (or primitive-like) types listed below, then simply assign the cloned object to the object to be cloned
+      if (typeof(T).GetTypeInfo().IsValueType) { return obj; }
+      switch (Type.GetTypeCode(typeof(T))) {
+        case TypeCode.Boolean:
+        case TypeCode.Byte:
+        case TypeCode.DateTime:
+        case TypeCode.Decimal:
+        case TypeCode.Double:
+        case TypeCode.Int16:
+        case TypeCode.Int32:
+        case TypeCode.Int64:
+        case TypeCode.SByte:
+        case TypeCode.Single:
+        case TypeCode.String:
+        case TypeCode.UInt16:
+        case TypeCode.UInt32:
+        case TypeCode.UInt64:
+          return obj;
+        default:
+          var clonedObj = Activator.CreateInstance<T>();
+          // Naive approach 1: JsonSerialize the original object and JsonDeserialize back to T
+          /*
+           * Causes an issue when T is an object with circular references or too deep of a nested structure
+          var jsonOptions = new JsonSerializerOptions() {
+            MaxDepth = 2,
+            IgnoreNullValues = true
+          };
+          clonedObj = JsonSerializer.Deserialize<T>(JsonSerializer.Serialize(obj, typeof(T), jsonOptions), jsonOptions);
+          */
+
+          // Naive approach 2: Use reflection to copy readable and writeable properties
+          // NOTE: This will copy references when performing the assignment for reference type objects
+          // Also, if the object to be cloned has read only fields that are assigned in the constructor, such as Ids, 
+          // the cloned object will not be able to set that property
+          CopyValues<T>(obj, ref clonedObj);
+          return clonedObj;
+      }
+    }
+
+    public static void CopyValues<T>(T fromObj, ref T toObj) {
+      if (typeof(T).GetTypeInfo().IsValueType) { toObj = fromObj; }
+      switch (Type.GetTypeCode(typeof(T))) {
+        case TypeCode.Boolean:
+        case TypeCode.Byte:
+        case TypeCode.DateTime:
+        case TypeCode.Decimal:
+        case TypeCode.Double:
+        case TypeCode.Int16:
+        case TypeCode.Int32:
+        case TypeCode.Int64:
+        case TypeCode.SByte:
+        case TypeCode.Single:
+        case TypeCode.String:
+        case TypeCode.UInt16:
+        case TypeCode.UInt32:
+        case TypeCode.UInt64:
+          toObj = fromObj;
+          break;
+        default:
+          var props = typeof(T).GetProperties();
+          foreach (var prop in props) {
+            if (!prop.CanRead || !prop.CanWrite) { continue; }
+            prop.SetValue(toObj, prop.GetValue(fromObj));
+          }
+          break;
+      }
+    }
+  }
+}
