5382087a9dfb34d4b84c95eafce1583f29b1d331
Humberto Cruz-Oyola Jr <i_am_listening@Hotmail.com>
2020-02-11 14:58:24 -0700

    Small update to sample to initialize list of Children on New for ItemToWrapGroup

--- DragAndDropComponent/DragAndDrop/Data/ItemToWrapGroup.cs
+++ DragAndDropComponent/DragAndDrop/Data/ItemToWrapGroup.cs
@@ -5,6 +5,6 @@ using System.Threading.Tasks;
 
 namespace DragAndDrop.Data {
   public class ItemToWrapGroup : ItemToWrapBase {
-    public List<ItemToWrapBase> Children { get; set; }
+    public List<ItemToWrapBase> Children { get; set; } = new List<ItemToWrapBase>();
   }
 }
